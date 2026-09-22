24043f194c38249d75f19f7837a0d0a1c6bc9985
Harshitha Rai <harshithanrai12@gmail.com>
2026-09-22 14:39:52 +0530

    Remove temporary Cloudinary test

--- server/index.js
+++ server/index.js
@@ -19,38 +19,7 @@ dbconnection();
 app.get("/apitest", (req, res) => {
   res.send("Rental Property API Running...");
 });
-app.get("/cloudinary-test", async (req, res) => {
-  try {
-    const cloudinary = require("./cloudinary");
 
-    const testImage =
-      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIHWP4z8DwHwAFgAI/7kT5WQAAAABJRU5ErkJggg==";
-
-    const result = await cloudinary.uploader.upload(testImage, {
-      folder: "rentease/test"
-    });
-
-    console.log("CLOUDINARY TEST SUCCESS:", result.secure_url);
-
-    res.send({
-      success: true,
-      message: "Cloudinary upload works",
-      url: result.secure_url
-    });
-  } catch (error) {
-    console.log("CLOUDINARY TEST ERROR:", {
-      message: error.message,
-      http_code: error.http_code,
-      name: error.name
-    });
-
-    res.status(500).send({
-      success: false,
-      message: error.message,
-      http_code: error.http_code
-    });
-  }
-});
 // routes (we will add later)
 app.use("/property", require("./Routes/PropertyRoutes"));
 app.use("/request", require("./Routes/RequestRoutes"));
